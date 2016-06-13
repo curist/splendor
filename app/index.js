cbcf0d1ca6699158df06219667498b55bd2ffe35
neil <neil@ghtinc.com>
2016-06-13 13:54:51 +0800

    add extract text plugin to build production bundle

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 var m = require('mithril');
 var B = require('app/broker');
 
+require('app/style.css');
 require('app/actions');
 
 var Home = require('app/views/Home');
---
+++ webpack.build.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: 'build',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'build'
+  },
+  resolve: {
+    alias: {
+      app: path.join(__dirname, 'app')
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader")
+      }
+    ]
+  },
+  postcss: function() {
+    return [precss, autoprefixer];
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'build by webpack @ ' + new Date(),
+      hash: true,
+      template: 'index.ejs'
+    }),
+    new ExtractTextPlugin('style.css')
+  ]
+};
