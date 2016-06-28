a5fface1fdac0287806ea195957bcd743fb779d9
neil <neil@ghtinc.com>
2016-06-28 12:22:49 +0800

    new game setting

--- app/views/NewGameSetting.js
+++ app/views/NewGameSetting.js
@@ -2,6 +2,10 @@ import m from 'mithril';
 import B from 'app/broker';
 import _ from 'underscore';
 
+const debug = require('debug')('app/views/NewGameSetting');
+
+// TODO import AIs here
+
 
 const NewGameSetting = {
   controller () {
@@ -10,6 +14,18 @@ const NewGameSetting = {
     ctrl.players = m.prop(2);
     ctrl.score = m.prop(15);
 
+    ctrl.playerSetting = m.prop([
+      'human',
+      'human',
+      'human',
+      'human',
+    ]);
+
+    ctrl.availablePlayers = [
+      'human',
+      'ai:easy',
+    ];
+
     ctrl.initGame = () => {
       B.do({
         action: 'game/init',
@@ -17,9 +33,15 @@ const NewGameSetting = {
         winGameScore: ctrl.score(),
       });
     };
+
+    ctrl.changePlayerSetting = (n, entity) => {
+      let playerSetting = ctrl.playerSetting();
+      playerSetting[n] = entity;
+      ctrl.playerSetting(playerSetting);
+    };
   },
   view (ctrl) {
-    return m('view', m('.col', [
+    return m('.NewGameSetting', m('.col', [
       m('.row', [
         'players: ',
         m('select', {
@@ -31,12 +53,25 @@ const NewGameSetting = {
           }, n);
         })),
       ]),
+      _.range(1, parseInt(ctrl.players()) + 1).map(n => {
+        return m('.row', [
+          m('.PlayerSetting', `player ${n}:`),
+          m('select', {
+            value: ctrl.playerSetting()[n-1],
+            onchange: m.withAttr('value', ctrl.changePlayerSetting.bind(ctrl, n-1)),
+          }, ctrl.availablePlayers.map(player => {
+            return m('option', {
+              value: player
+            }, player);
+          })),
+        ]);
+      }),
       m('.row', [
         'win game score: ',
         m('select', {
           value: ctrl.score(),
           onchange: m.withAttr('value', ctrl.score),
-        }, [15, 18, 20, 25, 30].map(n => {
+        }, [15, 20, 25, 30].map(n => {
           return m('option', {
             value: n
           }, n);
