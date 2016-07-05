ae697105b0641a79db85ed4f0aa59c79561ec6c1
neil <neil@ghtinc.com>
2016-07-05 16:20:15 +0800

    safer localstorage state parse

--- app/utils.js
+++ app/utils.js
@@ -1,11 +1,19 @@
 import db from 'app/db';
 import B from 'app/broker';
 
+function parse(s) {
+  try {
+    return JSON.parse(s);
+  } catch(e) {
+    return {};
+  }
+}
+
 export const syncLocalStorage = (function() {
   var key = 'mithril-splendor-app';
 
   function initStateFromLocalStorage() {
-    var state = JSON.parse(localStorage.getItem(key) || '{}');
+    var state = parse(localStorage.getItem(key));
     db.set(state);
     B.do({ action: 'app/data-inited' });
   }
