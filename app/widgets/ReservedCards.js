3862987930d8a9ad623c2f8fc980b12ec650f69f
neil <neil@ghtinc.com>
2016-07-05 13:44:14 +0800

    hide hold cards, when no holding cards

--- app/widgets/ReservedCards.js
+++ app/widgets/ReservedCards.js
@@ -7,7 +7,8 @@ import './reservedcards.css';
 const ReservedCards = {
   view (ctrl, player) {
     const cards = player.reservedCards;
-    return m('.ReservedCards', [
+    const hide = (cards.length == 0) ? '.hide' : '';
+    return m('.ReservedCards' + hide, [
       m('.Title', 'Hold: ' + cards.length),
       m('.CardContainer', cards.map(card => {
         return m(Card, card);
