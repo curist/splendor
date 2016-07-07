e1f06cc8921db546860d037af7235615041c32cc
neil <neil@ghtinc.com>
2016-07-07 11:48:12 +0800

    webpack loads web fonts

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -49,6 +49,10 @@ module.exports = {
         test: /\.(jpg|jpeg|png)$/,
         loader: 'file'
       },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file'
+      },
     ]
   },
   eslint: {
