fc5de94bbd700fb07b48cffff84c97b6a5ff27e8
curist <curist.cyc@gmail.com>
2016-08-11 07:33:58 +0800

    ???

--- app/AI/ai_apocalypse.js
+++ app/AI/ai_apocalypse.js
@@ -3,8 +3,8 @@ import {
   hasEnoughResourceForCard,
   flattenResources,
   zipResources,
-  playerBoughtCard,
 } from './helpers';
+import { canTakeNoble } from 'app/validates';
 
 const debug = require('debug')('app/AI/apocalypse');
 
@@ -28,6 +28,10 @@ function table(data, fields) {
   }
 }
 
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
 function countResources (resources) {
   return Object.keys(resources).reduce((total, k) => {
     return total + resources[k];
@@ -39,21 +43,6 @@ function getAffordableCards(player, cards) {
 }
 
 function cardCost(player, card) {
-  let shortOf = 0;
-  let cost = 0;
-  colors.forEach(color => {
-    var short = card[color]
-      - player.resources[color]
-      - player.bonus[color];
-    cost += Math.max(0, card[color] - player.bonus[color]);
-    if(short > 0) {
-      shortOf += short;
-    }
-  });
-  return shortOf + cost;
-}
-
-function realCardCost(player, card) {
   let cost = 0;
   colors.forEach(color => {
     let short = card[color] - player.bonus[color];
@@ -64,50 +53,60 @@ function realCardCost(player, card) {
   return cost;
 }
 
-function realCardCostWithPenalty(player, card) {
+function cardCostWithPenalty(player, card, resources) {
+  const turnsToTakeResources = Math.floor((10 - countResources(player.resources)) / 3);
   let cost = 0;
   let penalty = 0;
   colors.forEach(color => {
-    let short = card[color] - player.bonus[color];
-    if(short > 0) {
-      cost += short;
+    let colorCost = card[color] - player.bonus[color];
+    if(colorCost > 0) {
+      cost += colorCost;
     }
-    if(short > 3) {
-      penalty += (short - 3) * 10;
+    let short = colorCost - player.resources[color];
+    if(short > turnsToTakeResources) {
+      penalty += (short - turnsToTakeResources) * 3;
+    }
+    // unreachable
+    if(short > resources[color]) {
+      penalty += 100;
     }
   });
-  debug(`cost: ${cost}, penalty: ${penalty}`);
   return cost + penalty;
 }
 
-function cardCostWithPenalty(player, card) {
-  let shortOf = 0;
+function cardCostWithoutCurrentResource(player, card, resources) {
   let cost = 0;
   let penalty = 0;
   colors.forEach(color => {
-    var short = card[color]
-      - player.resources[color]
-      - player.bonus[color];
-    cost += Math.max(0, card[color] - player.bonus[color]);
-    if(cost > 4) {
-      penalty += Math.pow(3, (cost - 4));
+    let colorCost = card[color] - player.bonus[color];
+    if(colorCost > 0) {
+      cost += colorCost;
     }
-    if(short > 0) {
-      shortOf += short;
+    // unreachable
+    if(colorCost > resources[color]) {
+      penalty += 100;
     }
   });
-  debug(shortOf, cost, penalty);
-  return shortOf + cost + penalty;
+  return cost + penalty;
 }
 
-
-function cardPointsPerTurn(player, card) {
-  const cost = realCardCost(player, card);
+function cardPointsPerTurn(player, card, resources) {
+  const cost = cardCostWithPenalty(player, card, resources);
   return card.points / (1 + cost / 3);
 }
 
-function cardValue(player, state, cards, card) {
-  const { nobles } = state;
+function playerBoughtCard(player, card) {
+  let futurePlayer = clone(player);
+  futurePlayer.bonus[card.provides] += 1;
+  colors.forEach(color => {
+    const cost = card[color] - player.bonus[color];
+    futurePlayer.resources[color] -= Math.max(0, cost);
+  });
+  return futurePlayer;
+}
+
+function cardValue(player, state, cards, card, print) {
+  const { nobles, resources } = state;
   const { provides } = card;
 
   function cardNobleValue(player, noble) {
@@ -127,65 +126,211 @@ function cardValue(player, state, cards, card) {
   }
 
   function cardBoardValue(player, cardToValue) {
-    const futurePlayer = playerBoughtCard(player, card);
+    let futurePlayer = playerBoughtCard(player, cardToValue);
     const totalValue = cards.reduce((total, card) => {
       if(cardToValue.key == card.key) {
         return total;
       }
-      return total + cardPointsPerTurn(futurePlayer, card);
+      return total + cardPointsPerTurn(futurePlayer, card, resources);
     }, 0);
     return totalValue;
   }
 
-  function turnsToEndGame(player, card, pointsToGo) {
-    const futurePlayer = playerBoughtCard(player, card);
-    const points = cards.filter(cardo => {
+  // function turnsToEndGame(player, card, pointsToGo) {
+  //   let futurePlayer = playerBoughtCard(player, card);
+
+  //   const effectivePoints = cards.filter(cardo => {
+  //     return cardo.key !== card.key;
+  //   }).filter(card => {
+  //     return card.points > 0 && cardCostWithPenalty(player, card, resources) < 8;
+  //   }).map(card => {
+  //     return {
+  //       key: card.key,
+  //       ppt: cardPointsPerTurn(futurePlayer, card, resources),
+  //       turns: 1 + cardCostWithPenalty(player, card, resources) / 3,
+  //       points: card.points,
+  //     };
+  //   }).sort((cardA, cardB) => {
+  //     return cardB.ppt - cardA.ppt;
+  //   });
+
+  //   let turns = 0;
+  //   let score = 0;
+  //   let pointCards = [];
+  //   for(let i = 0, len = effectivePoints.length; i < len && score < pointsToGo ; i++) {
+  //     let effectivePoint = effectivePoints[i];
+  //     score += effectivePoint.points;
+  //     turns += effectivePoint.turns;
+  //     pointCards.push(effectivePoint);
+  //   }
+  //   debug(`ptg:${pointsToGo},turns:${turns}, score:${score}`);
+  //   let penalty = Math.max(pointsToGo - score, 0) * 10;
+  //   if(print) {
+  //     table(pointCards, ['key', 'ppt', 'turns', 'points']);
+  //   }
+  //   return turns + penalty;
+  // }
+
+  function turnsToEndGame(player, allCards, nobles, resources, card, pointsToGo) {
+    const cards = allCards.filter(cardo => {
       return cardo.key !== card.key;
+    });
+    // can't reach end game score, give penalties
+    if(cards.length == 0) {
+      let penalty = pointsToGo * 10;
+      return penalty;
+    }
+    // roughly estimate if cards on board can end game
+    const allCardPoints = allCards.reduce((total, card) => {
+      return total + card.points;
+    }, 0);
+    // can't reach end game score, give penalties
+    if(allCardPoints < pointsToGo) {
+      let penalty = (pointsToGo - allCardPoints) * 20;
+      return penalty;
+    }
+
+    const { provides } = card;
+    let futurePlayer = clone(player);
+    futurePlayer.bonus[provides] += 1;
+
+    let takeCardPoints = card.points;
+    let affordableNobleKey = -1;
+    const affordableNoble = nobles.find(noble => {
+      affordableNobleKey = noble.key;
+      return canTakeNoble(futurePlayer, noble);
+    });
+    let remainNobles = nobles;
+    if(affordableNoble) {
+      takeCardPoints += 3;
+
+      // filter out acquired noble
+      remainNobles = nobles.filter(noble => {
+        return noble.key !== affordableNobleKey;
+      });
+    }
+
+    // reach end game score, no further ado
+    if(pointsToGo - takeCardPoints <= 0) {
+      // not returing real end game turns
+      // we favor higher end game score
+      // return pointToGo - takeCardPoints;
+      // ^^^ or we check this outside of this function???
+
+      // buy this card to end game, take these turns
+      return 1 + cardCostWithoutCurrentResource(player, card, resources) / 3;
+    }
+
+    const cardsCanEndGameInTurns = cards.map(card => {
+      const turns = turnsToEndGame(futurePlayer, cards, remainNobles, resources, card, pointsToGo - takeCardPoints);
+      return turns;
+    }).filter(turns => {
+      return turns < 10;
+    }).sort();
+    if(cardsCanEndGameInTurns.length > 0) {
+      // sort them by minimal turns to end game
+      // if two cards can end game in same turns
+      // pick higher points one
+      return cardsCanEndGameInTurns[0];
+    }
+
+
+    const effectivePoints = cards.filter(cardo => {
+      return cardo.key !== card.key;
+    }).filter(card => {
+      return card.points > 0 && cardCostWithoutCurrentResource(player, card, resources) < 8;
     }).map(card => {
-      return cardPointsPerTurn(futurePlayer, card);
-    }).filter(point => {
-      return point > 0;
-    }).sort().reverse();
-    debug(points);
+      return {
+        key: card.key,
+        ppt: cardPointsPerTurn(futurePlayer, card, resources),
+        turns: 1 + cardCostWithoutCurrentResource(player, card, resources) / 3,
+        points: card.points,
+      };
+    }).sort((cardA, cardB) => {
+      return cardB.ppt - cardA.ppt;
+    });
 
     let turns = 0;
     let score = 0;
-    for(let i = 0, len = points.length; i < len || score >= pointsToGo ; i++) {
-      score += points[i];
-      turns++;
+    let pointCards = [];
+    for(let i = 0, len = effectivePoints.length; i < len && score < pointsToGo ; i++) {
+      let effectivePoint = effectivePoints[i];
+      score += effectivePoint.points;
+      turns += effectivePoint.turns;
+      pointCards.push(effectivePoint);
     }
-    let penalty = Math.max(pointsToGo - score, 0) * 100;
-    debug(`points to go: ${pointsToGo}, turns: ${turns}`);
+    debug(`ptg:${pointsToGo},turns:${turns}, score:${score}`);
+    let penalty = Math.max(pointsToGo - score, 0) * 10;
+    if(print) {
+      table(pointCards, ['key', 'ppt', 'turns', 'points']);
+    }
     return turns + penalty;
   }
 
   function valueForPlayer(player, card) {
-    const cost = realCardCostWithPenalty(player, card);
+    const costWithPenalty = cardCostWithPenalty(player, card, resources);
+    // const noblePoints = nobles.map(noble => {
+    //   return cardNobleValue(player, noble);
+    // }).sort().reverse();
 
-    const totalNobleValue = nobles.reduce((total, noble) => {
-      return total + cardNobleValue(player, noble);
-    }, 0);
-    const heuristicPoints = cardPointsPerTurn(player, card) + totalNobleValue;
-    debug(`per turn points: ${cardPointsPerTurn(player, card)}, noble: ${totalNobleValue}`);
-    const turns = turnsToEndGame(player, card, winGameScore - heuristicPoints);
-    return turns + cost;
+    // const actionScore = card.points + (noblePoints[0] || 0);
+    const pointsToGo = winGameScore - player.score;
+    const turns = turnsToEndGame(player, cards, nobles, resources, card, pointsToGo);
+
+    // debug(`k:${card.key},turns:${turns},penalty:${costWithPenalty/3},as:${actionScore}`);
+    return turns + Math.ceil(costWithPenalty / 3);
   }
 
   return valueForPlayer(player, card);
 }
 
+function reachableCardValue(player, cards, resources, cardToValue) {
+  const cost = cardCostWithPenalty(player, cardToValue, resources);
+  let futurePlayer = playerBoughtCard(player, cardToValue);
+
+  function cardBoardValue() {
+    const totalValue = cards.reduce((total, card) => {
+      if(cardToValue.key == card.key) {
+        return total;
+      }
+      return total + cardPointsPerTurn(futurePlayer, card, resources);
+    }, 0);
+    return totalValue;
+  }
+  return cardBoardValue() - cost;
+}
+
 function getBestCards(player, state, cards) {
-  const { nobles } = state;
+  if(cards.length == 0) {
+    return [];
+  }
+  const { nobles, resources } = state;
   const sortedCards = cards.map(card => {
     return Object.assign({
-      value: cardValue(player, state, cards, card)
+      turns: cardValue(player, state, cards, card)
     }, card);
   }).sort((cardA, cardB) => {
-    return cardA.value - cardB.value;
+    // smaller, the better
+    return cardA.turns - cardB.turns;
   });
-  let fields = ['rank', 'key', 'total_cost', 'value', 'provides'].concat(colors);
-  table(sortedCards, fields);
-  return sortedCards;
+  if(sortedCards[0].turns < 15) {
+    let fields = ['rank', 'key', 'total_cost', 'turns', 'provides'].concat(colors);
+    // debug('good sorted card');
+    table(sortedCards, fields);
+    cardValue(player, state, cards, sortedCards[0], true);
+    return sortedCards;
+  }
+  const reachableSortedCards = cards.map(card => {
+    return Object.assign({
+      value: reachableCardValue(player, cards, resources, card)
+    }, card);
+  }).sort((cardA, cardB) => {
+    return cardB.value - cardA.value;
+  });
+  let fields1 = ['rank', 'key', 'total_cost', 'points', 'value'].concat(colors);
+  // debug('reachable sorted card');
+  table(reachableSortedCards, fields1);
+  return reachableSortedCards;
 }
 
 function getBestCard(player, state, cards) {
@@ -221,7 +366,6 @@ export default class Normal {
     const affordableCards = getAffordableCards(player, allCards);
 
     // try to buy the best card
-    const card = getBestCard(player, state, allCards);
     const bestCards = getBestCards(player, state, allCards);
     const bestCard = bestCards[0];
     if(hasEnoughResourceForCard(player, bestCard)) {
