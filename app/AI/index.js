d6dbf2e66a8b70fb9978851b4b85414f56521044
nchang <nchang@ip-10-0-2-15.us-west-1.compute.internal>
2017-01-11 14:44:13 +0800

    clean up things
    add yarn

--- app/AI/index.js
+++ app/AI/index.js
@@ -1,15 +1,9 @@
 import Dumb from './ai_dumb';
 import Easy from './ai_easy';
 import Normal from './ai_normal';
-import MyGrandMa from './ai_my_grandma';
-import FiveYearsOld from './ai_fiveyearsold';
-import Apocalypse from './ai_apocalypse.js';
 
 module.exports = {
   dumb: Dumb,
   easy: Easy,
   normal: Normal,
-  MyGrandMa,
-  FiveYearsOld,
-  Apocalypse,
 };
